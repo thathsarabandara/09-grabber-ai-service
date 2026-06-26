a9813a8b0ff17bd6cedef2a57f8631cc68303d20
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-06-26 08:41:39 +0530

    feat: edit the main model init file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.operator import Operator
-from app.models.gesture import GestureMapping, GestureSetting
+from app.models.gesture import GestureMapping, GestureSetting, CustomGestureTemplate
 from app.models.voice import VoiceCommand, VoiceSetting
 from app.models.pick_place import PickPlaceSetting, GraspForce
 from app.models.sorting import SortingCategory, SortingRule
+from app.models.task_state import TaskState
